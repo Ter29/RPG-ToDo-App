27ab41f76121823b384149ba4ec0f4906f25c4e7
Ter29 <smarter2906@gmail.com>
2026-03-14 16:41:59 +0100

    add base frontend folders

--- backend/src/tests/ExampleService.test.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var ExampleService_1 = require("../services/ExampleService");
-describe("ExampleService", function () {
-    it("should return true", function () {
-        var service = new ExampleService_1.ExampleService();
-        expect(service.exampleMethod()).toBe(true);
-    });
-});
